08236da97d0791b13c62404681f4360634a7e892
Yoni <svechinskyy@gmail.com>
2019-03-14 21:33:35 +0200

    Add cors header support

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const router = express.Router();
+var cors = require("cors");
 
-const app = express();
-app.use('/', router);
-app.use(bodyParser.urlencoded({
-    extended: false,
-}));
+var app = express();
+app.use(cors());
+app.options("*", cors());
+app.use("/", router);
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: false,
-}));
-app.use(express.static(__dirname + '/public'));
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(express.static(__dirname + "/public"));
 
-require('./routes/Assets')(app);
+require("./routes/Assets")(app);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
-router.get('/api/hello', (req, res) => {
-    res.send({ msg: 'lynx backend' });
-})
+router.get("/api/hello", (req, res) => {
+  res.send({ msg: "lynx backend" });
+});
